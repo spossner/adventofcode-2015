628c7a6f8ceac4b045bf3bfcae012c83eec5b9f7
spossner <github@possner.de>
2021-11-29 08:32:25 +0100

    delivering presents by elves

---
+++ 20.py
@@ -0,0 +1,28 @@
+class Solution:
+    def solve(self, goal):
+        size = int(goal/10)+1
+        counts = [0] * size
+        for n in range(1, size):
+            step = n * 10
+            for i in range(n, size, n):
+                counts[i] += step
+        #print(counts)
+        for i in range(size):
+            if counts[i] >= goal:
+                return i
+        return -1
+
+
+if __name__ == '__main__':
+    s = Solution()
+
+    assert s.solve(10) == 1
+    assert s.solve(30) == 2
+    assert s.solve(40) == 3
+    assert s.solve(60) == 4
+    assert s.solve(70) == 4
+    assert s.solve(80) == 6
+    assert s.solve(120) == 6
+    assert s.solve(130) == 8
+    assert s.solve(150) == 8
+    print(s.solve(29000000))
